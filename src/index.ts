730c519f3ce4cab355394f122da941e624b10c09
YYYY <ymslucky@163.com>
2026-09-19 21:08:00 +0800

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -62,8 +62,8 @@ export default {
 					}),
 				),
 			    github: GithubProvider({
-			      clientID: await env.GITHUB_CLIENT_ID.get(),
-			      clientSecret: await env.GITHUB_CLIENT_SECRET.get(),
+			      clientID: env.GITHUB_CLIENT_ID.get(),
+			      clientSecret: env.GITHUB_CLIENT_SECRET.get(),
 			      scopes: ["user:email"],
 			    }),
 			},
